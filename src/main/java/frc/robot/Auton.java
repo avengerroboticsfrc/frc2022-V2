d97081ad29aa7e0ebf1a5c2ca85e331c3bd58f8a
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-03-17 17:58:53 -0400

    Idk

--- src/main/java/frc/robot/Auton.java
+++ src/main/java/frc/robot/Auton.java
@@ -1,17 +1,16 @@
 package frc.robot;
 
 import com.pathplanner.lib.PathPlanner;
-
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.RobotRamseteCommand;
+import frc.robot.commands.FridayRamseteCommand;
 import frc.robot.subsystems.DriveTrain;
 
 public class Auton extends SequentialCommandGroup {
     public Auton(DriveTrain drive) {
         Trajectory sixBallPath = PathPlanner.loadPath("New Path", 3, 5); 
         addCommands(
-            new RobotRamseteCommand(sixBallPath, drive)
+            new FridayRamseteCommand(sixBallPath, drive)
         );
     
 }
