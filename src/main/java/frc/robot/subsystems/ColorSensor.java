acae532f7b393fcc9f66917a2ff37b98bb17d995
Vineeth Pateel <95719853+Simgoo@users.noreply.github.com>
2022-03-19 14:08:14 -0400

    Index command and started on color sensor class

---
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+public class ColorSensor extends SubsystemBase {
+    
+}
