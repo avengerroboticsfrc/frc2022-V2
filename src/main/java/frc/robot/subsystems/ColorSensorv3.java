25e6ead61e2780e08509fdefd69099589b3d67ce
Vineeth Pateel <95719853+Simgoo@users.noreply.github.com>
2022-03-19 15:59:20 -0400

    Added a ColorSensor Class

---
+++ src/main/java/frc/robot/subsystems/ColorSensorv3.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ColorSensorv3 extends SubsystemBase {
+    
+    private ColorSensorv3 ColorSensor
+
+
+    public void getRedValue(){
+        ColorSensor.getRedValue();   
+    }
+
+    public void getBlueValue(){
+        ColorSensor.getBlueValue();
+    }
+}
