b37e186127f5f770fed2ec27f73a8311572d18c3
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-03-19 15:32:52 -0400

    Making Command parallel instead of sequential

--- src/main/java/frc/robot/commands/IndexIntake.java
+++ src/main/java/frc/robot/commands/IndexIntake.java
@@ -1,12 +1,12 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.Index;
 import frc.robot.subsystems.Intake;
 import frc.robot.commands.IndexCommand;
 import frc.robot.commands.IntakeCommand;
 
-public class IndexIntake extends SequentialCommandGroup {
+public class IndexIntake extends ParallelCommandGroup {
     public void IntakeIndex(Intake intake, int power, Index index) {
         addCommands(
             new IntakeCommand(intake, power),
