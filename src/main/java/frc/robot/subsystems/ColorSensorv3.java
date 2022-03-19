a1013675a1e4b0e5de55916924b546c09ba2b77f
Vineeth Pateel <95719853+Simgoo@users.noreply.github.com>
2022-03-19 16:10:32 -0400

    added a ;

--- src/main/java/frc/robot/subsystems/ColorSensorv3.java
+++ src/main/java/frc/robot/subsystems/ColorSensorv3.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ColorSensorv3 extends SubsystemBase {
     
-    private ColorSensorv3 ColorSensor
+    private ColorSensorv3 ColorSensor;
 
 
     public void getRedValue(){
