1c32422caf3b2a5fbfb3bb59d8e4d26adc8aa186
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-03-19 16:19:49 -0400

    2 color sensor class

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-
-public class ColorSensor extends SubsystemBase {
-    
-}
