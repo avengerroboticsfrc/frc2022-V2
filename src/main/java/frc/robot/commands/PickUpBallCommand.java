e031325adeedceda361ce2f3e463abfaf311665b
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2022-03-21 16:37:57 -0400

    CHanged Complex Command

--- src/main/java/frc/robot/commands/PickUpBallCommand.java
+++ src/main/java/frc/robot/commands/PickUpBallCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Index;
@@ -7,12 +8,12 @@ import frc.robot.subsystems.Intake;
 
 public class PickUpBallCommand extends SequentialCommandGroup {
 
-
-    public PickUpBallCommand(Intake intake, Index index, Double power, Intake Intakepneumatics) {
-        addCommands(
-            deadline(new PneumaticsCommand(Intakepneumatics)), new WaitCommand(1),
-            deadline(new IntakeCommand(intake, power), new IndexCommand(index, power))
-        );
+  public PickUpBallCommand(Intake intake, Index index, Double power) {
+    addCommands(
+        new InstantCommand(intake::extend),
+        deadline(new WaitCommand(2), new IntakeCommand(intake, power), new IndexCommand(index, power)),
+        new InstantCommand(intake::retract)
+    );
 
     
  }
