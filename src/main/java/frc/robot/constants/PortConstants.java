13e1011716f43bcb5168a642df3b10c3a1db6792
Karthik Hari <35894769+khari05@users.noreply.github.com>
2022-03-23 22:36:04 -0400

    update some bindings

--- src/main/java/frc/robot/constants/PortConstants.java
+++ src/main/java/frc/robot/constants/PortConstants.java
@@ -20,11 +20,10 @@ public final class PortConstants {
   public static final int HOOD_SERVO = 41;
   public static final int TURRET_TURN_MOTOR = 5;
 
-  public static final int[] LIFT_MOTORS = { 8, 9 };
-  public static final int[] ARM_MOTORS = { 7, 11 };
+  public static final int VERTICAL_LIFT_MOTORS = 8;
+  public static final int ARM_MOTOR = 7;
 
   public static final int[] INTAKE_PNEUMATICS_PORTS = { 0, 1 };
   public static final int INTAKE_MOTOR = 12;
   public static final int INDEX_MOTOR = 6;
-  public static final int ColorSensor = 3; //port will change
 }
