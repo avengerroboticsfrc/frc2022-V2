cc10bfcbe2b25dbb4764c3ac18c8e37b79e7184d
AvengerRobotics <admin@avengerrobotics.com>
2022-04-04 14:53:54 -0500

    idk at this point

--- src/main/java/frc/robot/commands/auton/TwoBallTimeBased.java
+++ src/main/java/frc/robot/commands/auton/TwoBallTimeBased.java
@@ -16,11 +16,11 @@ import frc.robot.subsystems.Shooter;
 public class TwoBallTimeBased extends SequentialCommandGroup {
     public TwoBallTimeBased(DriveTrain drive, Intake intake, Index index,  IntakeToIndex intakeToIndex, Shooter shooter, IndexToShooter indexToShooter, double intakePower, double indexPower, double intakeToIndexPower, double shooterPower, double indexToShooterPower, Limelight limelight){
         addCommands(
-            new RunCommand(() -> drive.tankDrive(.5, .5), drive).withTimeout(1.5),
+            new RunCommand(() -> drive.tankDrive(0.5, 0.5), drive).withTimeout(1.5),
             new WaitCommand(1),
             new RunCommand(() -> drive.tankDrive(0,0), drive),
             new PickUpBallCommandGroup(intake, intakeToIndex, index, intakePower, intakeToIndexPower, indexPower),
-            new RunCommand(() -> drive.tankDrive(.5, .5), drive).withTimeout(0.5),
+            new RunCommand(() -> drive.tankDrive(0.5, -0.5), drive).withTimeout(0.5),
             new ShootBallCommandGroup(shooter, index, indexToShooter, limelight, shooterPower, indexPower, indexToShooterPower).withTimeout(3)
         );
 
